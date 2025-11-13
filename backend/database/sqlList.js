1948cf74b8db231424f9be0318c158065e59b3a5
bluesparkheechan <blussignal@gmail.com>
2025-11-13 17:37:16 +0900

    박희찬/충돌누락복구/25-11-13

--- backend/database/sqlList.js
+++ backend/database/sqlList.js
@@ -2,19 +2,13 @@
 const boards = require("./sqls/board.js");
 const users = require("./sqls/user.js");
 const consult = require("./sqls/consult.js");
-<<<<<<< HEAD
 const signUp = require("./sqls/signup.js");
 
-=======
 const inquiry = require("./sqls/inquiry.js");
->>>>>>> Branch-PHC
 module.exports = {
   ...boards,
   ...users,
   ...consult,
-<<<<<<< HEAD
-  ...signUp,
-=======
   ...inquiry,
->>>>>>> Branch-PHC
+  ...signUp,
 };
